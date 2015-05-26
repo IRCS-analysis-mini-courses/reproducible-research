4e190ca1e77c464b1e0920e39633cedd45dab5ca
julian3rd <julianj3rd@gmail.com>
2015-05-25 23:22:50 -0400

    updated importation tutorial script

--- Day-3/notebooks/importing-data.R
+++ Day-3/notebooks/importing-data.R
@@ -1,5 +1,6 @@
 #!/usr/bin/env Rscript
 
+library(readr)
 
 # importing 2010 US Census data with base R -------------------------------
 
@@ -37,16 +38,45 @@ census.data.readr <- read_csv(census.url, col_names = TRUE, skip = 3)
 
 # import program effort data using base R ---------------------------------
 
+# notice that R parses the DAT file irregular spacing properly
+# on the first try but we need to supply correct column names
+
 program.effort.url <- 'http://data.princeton.edu/wws509/datasets/effort.dat'
 file.dir <- '~/GitHub/reproducible-research/Day-3'
 program.effort.file <- 'program-effort-from-r.csv'
 
 program.effort.data  <- 
-  download.file(url = program.effor.url, 
+  download.file(url = program.effort.url, 
                 destfile = file.path(file.dir, program.effort.file))
 
 program.effort.base <- 
-  read.csv(file.path(file.dir, program.effort.file))
-           stringsAsFactors = FALSE, header = TRUE)
+read.table(file.path(file.dir, program.effort.file),
+         stringsAsFactors = FALSE, header = TRUE)
 
 View(program.effort.base)
+
+# change column names
+
+program.columns <- c('Country', 'Setting', 'Effort', 'Change')
+
+program.effort.base <- 
+  read.table(file.path(file.dir, program.effort.file),
+             stringsAsFactors = FALSE, header = TRUE, 
+             col.names = program.columns)
+
+View(program.effort.base)
+
+
+# import program effort data using readr ----------------------------------
+
+# notice that reader does not properly read the variable spacing
+
+program.effort.readr  <- read_table(program.effort.url)
+
+View(program.effort.readr)
+
+# import data again with variable whitespace
+
+program.effort.readr  <- read_table(program.effort.url)
+
+View(program.effort.readr)
