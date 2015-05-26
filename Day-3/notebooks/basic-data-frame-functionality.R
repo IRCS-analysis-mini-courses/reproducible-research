c92600aff28c7ba79b862482262bdaaedfb1535c
julian3rd <julianj3rd@gmail.com>
2015-05-26 09:24:15 -0400

    updated R data frame functionality R script

--- Day-3/notebooks/basic-data-frame-functionality.R
+++ Day-3/notebooks/basic-data-frame-functionality.R
@@ -19,6 +19,9 @@ census.data.base <-
   read.csv(file.path(file.dir, census.file), stringsAsFactors = FALSE, 
            header = TRUE, skip = 3)
 
+attributes(census.data.base)
+class(census.data.base)
+
 density.data.dimen <- dim(census.data.base)
 
 density.data.cols <- colnames(census.data.base)
@@ -49,10 +52,45 @@ print(paste('Name:','Index', sep = ' '))
 apply(data.frame(density.data.cols, which.idx), 1, 
       function(row) {cat(row, sep = ': ', fill  = 20) })
 
-
 # basic functionality with the Hadleyverse --------------------------------
 
+census.data.readr <- read_csv(census.url, col_names = TRUE, skip = 3)
 
+# data.frames in the Hadleyverse have added fuctionality;
+# many more options are available to the class 'tbl_df'
+
+attributes(census.data.readr)
+class(census.data.readr)
+
+density.data.dimen.readr <- dim(census.data.readr)
+
+density.data.cols.readr <- colnames(census.data.readr)
+
+density.data.idx.readr <- row.names(census.data.readr)
+
+# note that class 'tbl_df' can take columns beginning with a
+# numeric argument as valid; this will not work when 
+# performing statistical tests
+
+pop.1910.readr <- census.data.readr[ ,'1910_POPULATION']
+pop.1910.readr
+
+sprintf('Data dimensions: %d rows, %d columns',  
+        density.data.dimen.readr[1], density.data.dimen.readr[2])
+
+cat('First 10 indices of population density data.frame:',
+    row.names(census.data.readr)[seq(1,10,1)])
+
+cat('Names of tbl_df columns:', density.data.cols.readr, fill = 15)
+
+# get indices using which()
+
+which.idx.readr <- which(names(census.data.readr) %in% density.data.cols.readr)
+
+print(paste('Name:','Index', sep = ' '))
+
+apply(data.frame(density.data.cols.readr, which.idx.readr), 1, 
+      function(row) {cat(row, sep = ': ', fill  = 20) })
 
 # data.frame methods ------------------------------------------------------
 
